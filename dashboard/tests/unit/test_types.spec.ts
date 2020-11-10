b7ce92f9bb77af43703dbecdef303cf02fe5d53f
Leland Boeman <lboeman@email.arizona.edu>
2020-11-10 11:38:56 -0700

    linttt

--- dashboard/tests/unit/test_types.spec.ts
+++ dashboard/tests/unit/test_types.spec.ts
@@ -11,13 +11,13 @@ test("Instantiate base system", () => {
   expect(system instanceof System).toBeTruthy();
   const inverters = system.inverters;
   expect(inverters instanceof Array).toBeTruthy();
-  for (var i = 0; i < inverters.length; i++) {
+  for (let i = 0; i < inverters.length; i++) {
     const inverter = inverters[i];
     expect(inverter instanceof Inverter).toBeTruthy();
     expect(typeof inverter.make_model).toBe("string");
     const arrays = inverter.arrays;
     expect(arrays instanceof Array).toBeTruthy();
-    for (var i = 0; i < arrays.length; i++) {
+    for (let i = 0; i < arrays.length; i++) {
       const array = arrays[i];
       expect(array instanceof PVArray).toBeTruthy();
       expect(
