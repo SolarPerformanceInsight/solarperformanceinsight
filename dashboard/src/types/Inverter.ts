cf0cddd89d23ec5ea012e1b51c61245b15a8c18a
Leland Boeman <lboeman@email.arizona.edu>
2020-11-19 10:36:54 -0700

    inverter constructor

--- dashboard/src/types/Inverter.ts
+++ dashboard/src/types/Inverter.ts
@@ -8,25 +8,36 @@ export class Inverter {
   name: string;
   make_model: string;
   inverter_parameters: PVSystInverterParameters | PVWattsInverterParameters;
-  losses_parameters: Object;
+  losses_parameters: object; // eslint-disable-line
   arrays: Array<PVArray>;
 
   constructor({
     name = "New Inverter",
     make_model = "ABC 520",
-    inverter_parameters = new PVSystInverterParameters(),
+    inverter_parameters = new PVSystInverterParameters({}),
     losses_parameters = {},
     arrays = []
-  } = {}) {
+  }: Partial<Inverter>) {
     this.name = name;
     this.make_model = make_model;
     this.inverter_parameters = inverter_parameters;
     this.losses_parameters = losses_parameters;
 
     if (arrays.length == 0) {
-      this.arrays = arrays as PVArray[];
+      this.arrays = [];
     } else {
       this.arrays = arrays.map(a => new PVArray(a));
     }
+  }
+
+  static isInstance(obj: any): obj is Inverter {
+    const maybe = obj as Inverter;
+    return (
+      maybe.name != undefined &&
+      maybe.make_model != undefined &&
+      maybe.inverter_parameters != undefined &&
+      maybe.losses_parameters != undefined &&
+      maybe.arrays != undefined
+    );
   }
 }
