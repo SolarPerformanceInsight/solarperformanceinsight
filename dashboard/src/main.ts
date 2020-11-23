c3c36e558ff72077c882ea929895d15a29c30e10
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-11-23 16:25:28 -0700

    add sentry to dashboard

--- dashboard/src/main.ts
+++ dashboard/src/main.ts
@@ -1,8 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
 
 Vue.config.productionTip = false;
+
+Sentry.init({
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+  integrations: [new VueIntegration({ Vue })],
+  // eslint-disable-next-line no-unused-vars
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id });
+    }
+    return event;
+  }
+});
 
 new Vue({
   router,
