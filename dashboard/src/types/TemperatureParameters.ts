acc4e54eaf05d5a9f48a1ce5a95c359fee11209a
Leland Boeman <lboeman@email.arizona.edu>
2020-11-30 11:35:38 -0700

    update temperature params

--- dashboard/src/types/TemperatureParameters.ts
+++ dashboard/src/types/TemperatureParameters.ts
@@ -2,10 +2,19 @@ export class PVSystTemperatureParameters {
   /* Currently only supports pvsyst model */
   u_c: number; // pvlib "Freestanding" default insulated is 15.0
   u_v: number; //        freestanding:00, insulated: 0.0
+  eta_m: number;
+  alpha_absorption: number;
 
-  constructor({ u_c = 29.0, u_v = 0.0 }: Partial<PVSystTemperatureParameters>) {
+  constructor({
+    u_c = 29.0,
+    u_v = 0.0,
+    eta_m = 0.1,
+    alpha_absorption = 0.9
+  }: Partial<PVSystTemperatureParameters>) {
     this.u_c = u_c;
     this.u_v = u_v;
+    this.eta_m = eta_m;
+    this.alpha_absorption = alpha_absorption;
   }
   static isInstance(obj: any): obj is PVSystTemperatureParameters {
     const maybe = obj as PVSystTemperatureParameters;
