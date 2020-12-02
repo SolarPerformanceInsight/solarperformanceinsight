25d539d7377ff3772a08d0eb34d86459cb440763
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-12-02 12:40:51 -0700

    add update

--- api/solarperformanceinsight_api/routers/systems.py
+++ api/solarperformanceinsight_api/routers/systems.py
@@ -1,7 +1,3 @@
-import random
-import uuid
-
-
 from typing import List
 from fastapi import APIRouter, Response, Request, Depends
 from pydantic.types import UUID
@@ -15,67 +11,6 @@ from ..storage import StorageInterface
 router = APIRouter()
 
 
-def _gen_uuid():
-    return uuid.uuid1(
-        node=random.SystemRandom().getrandbits(48),
-        clock_seq=random.SystemRandom().getrandbits(14),
-    )
-
-
-# PVSYSTEMS = {
-#     uuid.UUID("6b61d9ac-2e89-11eb-be2a-4dc7a6bcd0d9"): models.StoredPVSystem(
-#         system_id=uuid.UUID("6b61d9ac-2e89-11eb-be2a-4dc7a6bcd0d9"),
-#         name="Test PV System",
-#         latitude=33.98,
-#         longitude=-115.323,
-#         elevation=2300,
-#         albedo=0.2,
-#         inverters=[
-#             models.Inverter(
-#                 name="Inverter 1",
-#                 make_model="ABB__MICRO_0_25_I_OUTD_US_208__208V_",
-#                 inverter_parameters=models.SandiaInverterParameters(
-#                     Pso=2.08961,
-#                     Paco=250,
-#                     Pdco=259.589,
-#                     Vdco=40,
-#                     C0=-4.1e-05,
-#                     C1=-9.1e-05,
-#                     C2=0.000494,
-#                     C3=-0.013171,
-#                     Pnt=0.075,
-#                 ),
-#                 losses={},
-#                 arrays=[
-#                     models.PVArray(
-#                         name="Array 1",
-#                         make_model="Canadian_Solar_Inc__CS5P_220M",
-#                         modules_per_string=7,
-#                         strings=5,
-#                         tracking=models.FixedTracking(
-#                             tilt=20.0,
-#                             azimuth=180.0,
-#                         ),
-#                         temperature_model_parameters=models.PVsystTemperatureParameters(),  # NOQA: E501
-#                         module_parameters=models.PVsystModuleParameters(
-#                             alpha_sc=0.004539,
-#                             gamma_ref=1.2,
-#                             mu_gamma=-0.003,
-#                             I_L_ref=5.11426,
-#                             I_o_ref=8.10251e-10,
-#                             R_sh_ref=381.254,
-#                             R_s=1.06602,
-#                             R_sh_0=400.0,
-#                             cells_in_series=96,
-#                         ),
-#                     )
-#                 ],
-#             ),
-#         ],
-#     )
-# }
-
-
 @router.get(
     "/", response_model=List[models.StoredPVSystem], responses=default_get_responses
 )
@@ -83,25 +18,34 @@ async def list_systems(
     storage: StorageInterface = Depends(StorageInterface),
 ) -> List[models.StoredPVSystem]:
     """List available PV systems"""
-    with storage.start_transaction():
-        out = storage.list_systems()
+    with storage.start_transaction() as st:
+        out = st.list_systems()
         return out
+
+
+system_links = {
+    "Get System": {
+        "operationId": "get_system_systems__system_id__get",
+        "parameters": {"system_id": "$response.body#/system_id"},
+    },
+    "Delete System": {
+        "operationId": "delete_system_systems__system_id__delete",
+        "parameters": {"system_id": "$response.body#/system_id"},
+    },
+    "Update System": {
+        "operationId": "update_system_systems__system_id__post",
+        "parameters": {"system_id": "$response.body#/system_id"},
+    },
+}
 
 
 @router.post(
     "/",
-    response_model=models.CreatedPVSystemID,
+    response_model=models.PVSystemID,
     responses={
         **default_get_responses,
         409: {},
-        201: {
-            "links": {
-                "Get System": {
-                    "operationId": "get_system_systems__system_id__get",
-                    "parameters": {"system_id": "$response.body#/system_id"},
-                }
-            }
-        },
+        201: {"links": system_links},
     },
     status_code=201,
 )
@@ -110,10 +54,10 @@ async def create_system(
     response: Response,
     request: Request,
     storage: StorageInterface = Depends(StorageInterface),
-) -> models.CreatedPVSystemID:
+) -> models.PVSystemID:
     """Create a new PV System"""
-    with storage.start_transaction():
-        id_ = storage.create_system(system.name, system)
+    with storage.start_transaction() as st:
+        id_ = st.create_system(system)
         response.headers["Location"] = request.url_for(
             "get_system", system_id=id_.system_id
         )
@@ -123,13 +67,16 @@ async def create_system(
 @router.get(
     "/{system_id}",
     response_model=models.StoredPVSystem,
-    responses=default_get_responses,
+    responses={
+        **default_get_responses,
+        200: {"links": system_links},
+    },
 )
 async def get_system(
     system_id: UUID, storage: StorageInterface = Depends(StorageInterface)
 ) -> models.StoredPVSystem:
-    with storage.start_transaction():
-        return storage.get_system(system_id)
+    with storage.start_transaction() as st:
+        return st.get_system(system_id)
 
 
 @router.delete(
@@ -138,5 +85,30 @@ async def get_system(
 async def delete_system(
     system_id: UUID, storage: StorageInterface = Depends(StorageInterface)
 ):
-    with storage.start_transaction():
-        storage.delete_system(system_id)
+    with storage.start_transaction() as st:
+        st.delete_system(system_id)
+
+
+@router.post(
+    "/{system_id}",
+    response_model=models.PVSystemID,
+    responses={
+        **default_get_responses,
+        201: {"links": system_links},
+    },
+    status_code=201,
+)
+async def update_system(
+    system_id: UUID,
+    system: models.PVSystem,
+    response: Response,
+    request: Request,
+    storage: StorageInterface = Depends(StorageInterface),
+) -> models.PVSystemID:
+    """Create a new PV System"""
+    with storage.start_transaction() as st:
+        st.update_system(system_id, system)
+        response.headers["Location"] = request.url_for(
+            "get_system", system_id=system_id
+        )
+        return system_id
--- api/solarperformanceinsight_api/routers/tests/test_systems.py
+++ api/solarperformanceinsight_api/routers/tests/test_systems.py
@@ -1,4 +1,6 @@
-"""Test systems endpoints"""
+"""Test some select system endpoint interaction. The majority of testing
+is done via schemathesis in ../../tests/test_app.py
+"""
 import uuid
 
 
@@ -7,6 +9,7 @@ import pytest
 
 
 from solarperformanceinsight_api.main import app
+from solarperformanceinsight_api import models
 
 
 @pytest.fixture(scope="module")
@@ -21,6 +24,11 @@ def noauthclient():
     return TestClient(app)
 
 
+def test_get_system(client, system_id, stored_system, add_example_db_data):
+    response = client.get(f"/systems/{system_id}")
+    assert models.StoredPVSystem(**response.json()) == stored_system
+
+
 def test_get_system_404(client):
     id_ = str(uuid.uuid1())
     response = client.get(f"/systems/{id_}")
@@ -31,3 +39,20 @@ def test_get_system_noauth(noauthclient):
     id_ = str(uuid.uuid1())
     response = noauthclient.get(f"/systems/{id_}")
     assert response.status_code == 403
+
+
+def test_get_other_system(client, add_example_db_data, other_system_id):
+    response = client.get(f"/systems/{other_system_id}")
+    assert response.status_code == 404
+
+
+def test_delete_other_system(client, add_example_db_data, other_system_id):
+    response = client.delete(f"/systems/{other_system_id}")
+    assert response.status_code == 404
+
+
+def test_update_other_system(client, add_example_db_data, other_system_id, system_def):
+    system_def.albedo = 999
+    system_def.name = "New Name"
+    response = client.post(f"/systems/{other_system_id}", data=system_def.json())
+    assert response.status_code == 404
