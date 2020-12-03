2c44e2370c813ba10db7fed29d5a15587dfeba46
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-12-03 07:50:46 -0700

    remove redundant api test

--- api/solarperformanceinsight_api/tests/test_app.py
+++ api/solarperformanceinsight_api/tests/test_app.py
@@ -1,6 +1,3 @@
-from hypothesis import HealthCheck
-from hypothesis import settings as hypsettings
-from hypothesis import strategies as st
 from hypothesis.stateful import run_state_machine_as_test
 import pytest
 import schemathesis
@@ -10,21 +7,6 @@ from solarperformanceinsight_api.main import app
 
 
 schemathesis.fixups.install()
-schema = schemathesis.from_asgi("/openapi.json", app)
-
-
-@schema.parametrize()
-@schema.given(data=st.data())
-@hypsettings(max_examples=30, suppress_health_check=HealthCheck.all())
-def test_api(data, case, auth_token):
-    """Tests all endpoints in the OpenAPI Schema"""
-    if data.draw(st.booleans()):
-        case.headers["Authorization"] = f"Bearer {auth_token}"
-    else:
-        if "Authorization" in case.headers:
-            del case.headers["Authorization"]
-    response = case.call_asgi()
-    case.validate_response(response)
 
 
 @pytest.fixture()
