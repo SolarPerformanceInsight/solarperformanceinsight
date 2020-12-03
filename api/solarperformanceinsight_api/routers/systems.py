4a36425f51fdfa6d74827d533160d2ed8eaa57c3
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-12-03 07:50:54 -0700

    tweak system stubs for new models

--- api/solarperformanceinsight_api/routers/systems.py
+++ api/solarperformanceinsight_api/routers/systems.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import random
 import uuid
 
@@ -24,53 +25,57 @@ def _gen_uuid():
 PVSYSTEMS = {
     uuid.UUID("6b61d9ac-2e89-11eb-be2a-4dc7a6bcd0d9"): models.StoredPVSystem(
         system_id=uuid.UUID("6b61d9ac-2e89-11eb-be2a-4dc7a6bcd0d9"),
-        name="Test PV System",
-        latitude=33.98,
-        longitude=-115.323,
-        elevation=2300,
-        albedo=0.2,
-        inverters=[
-            models.Inverter(
-                name="Inverter 1",
-                make_model="ABB__MICRO_0_25_I_OUTD_US_208__208V_",
-                inverter_parameters=models.SandiaInverterParameters(
-                    Pso=2.08961,
-                    Paco=250,
-                    Pdco=259.589,
-                    Vdco=40,
-                    C0=-4.1e-05,
-                    C1=-9.1e-05,
-                    C2=0.000494,
-                    C3=-0.013171,
-                    Pnt=0.075,
+        created_at="2020-12-01T01:23:00+00:00",
+        modified_at="2020-12-01T01:23:00+00:00",
+        definition=models.PVSystem(
+            name="Test PV System",
+            latitude=33.98,
+            longitude=-115.323,
+            elevation=2300,
+            albedo=0.2,
+            inverters=[
+                models.Inverter(
+                    name="Inverter 1",
+                    make_model="ABB__MICRO_0_25_I_OUTD_US_208__208V_",
+                    inverter_parameters=models.SandiaInverterParameters(
+                        Pso=2.08961,
+                        Paco=250,
+                        Pdco=259.589,
+                        Vdco=40,
+                        C0=-4.1e-05,
+                        C1=-9.1e-05,
+                        C2=0.000494,
+                        C3=-0.013171,
+                        Pnt=0.075,
+                    ),
+                    losses={},
+                    arrays=[
+                        models.PVArray(
+                            name="Array 1",
+                            make_model="Canadian_Solar_Inc__CS5P_220M",
+                            modules_per_string=7,
+                            strings=5,
+                            tracking=models.FixedTracking(
+                                tilt=20.0,
+                                azimuth=180.0,
+                            ),
+                            temperature_model_parameters=models.PVsystTemperatureParameters(),  # NOQA: E501
+                            module_parameters=models.PVsystModuleParameters(
+                                alpha_sc=0.004539,
+                                gamma_ref=1.2,
+                                mu_gamma=-0.003,
+                                I_L_ref=5.11426,
+                                I_o_ref=8.10251e-10,
+                                R_sh_ref=381.254,
+                                R_s=1.06602,
+                                R_sh_0=400.0,
+                                cells_in_series=96,
+                            ),
+                        )
+                    ],
                 ),
-                losses={},
-                arrays=[
-                    models.PVArray(
-                        name="Array 1",
-                        make_model="Canadian_Solar_Inc__CS5P_220M",
-                        modules_per_string=7,
-                        strings=5,
-                        tracking=models.FixedTracking(
-                            tilt=20.0,
-                            azimuth=180.0,
-                        ),
-                        temperature_model_parameters=models.PVsystTemperatureParameters(),  # NOQA: E501
-                        module_parameters=models.PVsystModuleParameters(
-                            alpha_sc=0.004539,
-                            gamma_ref=1.2,
-                            mu_gamma=-0.003,
-                            I_L_ref=5.11426,
-                            I_o_ref=8.10251e-10,
-                            R_sh_ref=381.254,
-                            R_s=1.06602,
-                            R_sh_0=400.0,
-                            cells_in_series=96,
-                        ),
-                    )
-                ],
-            ),
-        ],
+            ],
+        ),
     )
 }
 
@@ -103,7 +108,10 @@ async def create_system(system: models.PVSystem, response: Response, request: Re
     """Create a new PV System"""
     global PVSYSTEMS
     id_ = _gen_uuid()
-    created = models.StoredPVSystem(system_id=id_, **system.dict())
+    now = dt.datetime.utcnow().replace(tzinfo=dt.timezone.utc, microsecond=0)
+    created = models.StoredPVSystem(
+        system_id=id_, created_at=now, modified_at=now, definition=system
+    )
     PVSYSTEMS[id_] = created
     response.headers["Location"] = request.url_for("get_system", system_id=id_)
     return created
