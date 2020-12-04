7af64b0fa2c5727a5d66e944210a8b55e01407ab
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-12-04 12:26:36 -0700

    Update python to speak w/ db
    Squashed commit of the following:
    
    commit f95388336d099f8a57a66bc7deea99e54375b8ae
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Fri Dec 4 12:13:02 2020 -0700
    
        use object id
    
    commit ca2204d90dc1eb1812938ac6a4c14927fdac9545
    Merge: c041ac5 fe236bb
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Fri Dec 4 11:21:07 2020 -0700
    
        Merge branch 'main' into dbinterface
    
    commit c041ac5babbc648df5b6e951e841b8da607cda6d
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 15:22:28 2020 -0700
    
        no cover userid
    
    commit 4feff330362d8778dc20d882d7e17b51d6ff2934
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 14:16:55 2020 -0700
    
        only on main branch
    
    commit 34218f4ec722ec7dd79a10db03fcb86f7c7c7e52
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 14:11:48 2020 -0700
    
        try adding db for py test
    
    commit 6e96ea3b802c595aca3b7b56a96bb9f39c16b535
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 14:06:00 2020 -0700
    
        add update test
    
    commit ce3f4a8f0b7c8d4917040d7d03bada8f36618916
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 13:25:10 2020 -0700
    
        add specific examples to openapi
    
    commit 7cc60f9f2a1e1a85b2001a96ddfca50a42cf4b8b
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 13:24:46 2020 -0700
    
        remove unecessary test
    
    commit f1093be19612f85ea73cd67ea7fc4dd4d176dbfd
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 12:42:45 2020 -0700
    
        update requirements
    
    commit 3b2f0239aded2556819fa6189755102d30392665
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 12:42:34 2020 -0700
    
        add example data and tweak testing app
    
    commit 24f955f3d536f09291bfc75bf44c22fb45f53a03
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 12:42:02 2020 -0700
    
        add storage tests
    
    commit 45c5972bd20f66279ef8023164cf25922efc1963
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 12:41:42 2020 -0700
    
        add update, adjust storage
    
    commit 25d539d7377ff3772a08d0eb34d86459cb440763
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Wed Dec 2 12:40:51 2020 -0700
    
        add update
    
    commit 652602c4f1ed7736751d583b00c32fdb1b9c992e
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Tue Dec 1 17:51:15 2020 -0700
    
        don't used constrained types
    
    commit 70f7a7d0aa7e0c0e39de3e279909bcc6f4226f68
    Author: Tony Lorenzo <atlorenzo@email.arizona.edu>
    Date:   Tue Dec 1 16:41:44 2020 -0700
    
        start db connection

--- api/solarperformanceinsight_api/tests/test_app.py
+++ api/solarperformanceinsight_api/tests/test_app.py
@@ -1,3 +1,4 @@
+from hypothesis import settings as hypsettings
 from hypothesis.stateful import run_state_machine_as_test
 import pytest
 import schemathesis
@@ -6,11 +7,17 @@ import schemathesis
 from solarperformanceinsight_api.main import app
 
 
+pytestmark = pytest.mark.usefixtures("add_example_db_data")
 schemathesis.fixups.install()
 
 
 @pytest.fixture()
-def auth_state_machine(auth_token):
+def new_settings():
+    return {"stateful_step_count": 3, "max_examples": 100}
+
+
+@pytest.fixture()
+def auth_state_machine(auth_token, new_settings):
     schema = schemathesis.from_asgi("/openapi.json", app)
 
     class APIWorkflow(schema.as_state_machine()):
@@ -22,13 +29,19 @@ def auth_state_machine(auth_token):
         def get_call_kwargs(self, case):
             return {"headers": self.headers}
 
+    APIWorkflow.TestCase.settings = hypsettings(
+        APIWorkflow.TestCase.settings, **new_settings
+    )
+
     return APIWorkflow
 
 
 @pytest.fixture()
-def noauth_state_machine():
+def noauth_state_machine(new_settings):
     schema = schemathesis.from_asgi("/openapi.json", app)
-    return schema.as_state_machine()
+    out = schema.as_state_machine()
+    out.TestCase.settings = hypsettings(out.TestCase.settings, **new_settings)
+    return out
 
 
 def test_statefully_with_auth(auth_state_machine):
