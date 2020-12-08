235fc840ebaa52eeca13ccf3fe84870db05b71cd
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-12-08 16:45:40 -0700

    add functions to construct pvlib ModelChains from a model.PVSystem
    for use by a check function and later to actually perform the modelling

---
+++ api/solarperformanceinsight_api/pvmodeling.py
@@ -0,0 +1,95 @@
+"""Module to translate models.py into pvlib objects and to run the
+pvlib models.
+"""
+from typing import Union, Tuple, Any, Dict, List
+
+
+from pvlib.location import Location  # type: ignore
+from pvlib.modelchain import ModelChain  # type: ignore
+from pvlib.pvsystem import PVSystem  # type: ignore
+from pvlib.tracking import SingleAxisTracker  # type: ignore
+
+from . import models
+
+
+def construct_location(system: models.PVSystem) -> Location:
+    """Construct a pvlib Location object from a PVSystem"""
+    return Location(
+        latitude=system.latitude,
+        longitude=system.longitude,
+        altitude=system.elevation,
+        name=system.name,
+    )
+
+
+def construct_tracker(
+    tracking: Union[models.FixedTracking, models.SingleAxisTracking]
+) -> Tuple[Union[PVSystem, SingleAxisTracker], Dict[str, Any]]:
+    """Return the appropriate pvlib.pvsystem class and tracking keywords
+    depending on the type of tracking unit"""
+    if isinstance(tracking, models.FixedTracking):
+        return PVSystem, dict(
+            surface_tilt=tracking.tilt, surface_azimuth=tracking.azimuth
+        )
+    elif isinstance(tracking, models.SingleAxisTracking):
+        return SingleAxisTracker, dict(
+            axis_tilt=tracking.axis_tilt,
+            axis_azimuth=tracking.axis_azimuth,
+            gcr=tracking.gcr,
+            backtrack=tracking.backtracking,
+        )
+    else:
+        raise TypeError("Only FixedTracking and SingleAxisTracking currently supported")
+
+
+def construct_pvsystem(
+    inverter: models.Inverter, albedo: float
+) -> Union[PVSystem, SingleAxisTracker]:
+    """Construct a pvlib.pvsystem.PVSystem (or SingleAxisTracker) from an
+    Inverter object"""
+    # only a single array for now until pvlib#1076
+    # otherwise one pvsystem/pvarray per array
+    array = inverter.arrays[0]
+    common_kwargs = dict(
+        albedo=albedo,
+        module=array.make_model,
+        module_parameters=array.module_parameters.dict(),
+        temperature_model_parameters=array.temperature_model_parameters.dict(),
+        modules_per_string=array.modules_per_string,
+        strings_per_inverter=array.strings,
+        inverter=inverter.make_model,
+        inverter_parameters=inverter.inverter_parameters.dict(),
+        name=inverter.name,
+    )
+    if inverter.losses is not None:
+        common_kwargs["losses_parameters"] = inverter.losses.dict()
+    system_model, tracking_kwargs = construct_tracker(array.tracking)
+    return system_model(**tracking_kwargs, **common_kwargs)
+
+
+def extract_pvlib_models(inverter: models.Inverter) -> Dict[str, str]:
+    """Extract the pvlib.modelchain.ModelChain _model keywords from an
+    Inverter definition"""
+    models = dict(inverter._pvlib_models)
+    models.update(
+        {
+            k: v
+            for k, v in inverter.dict().items()
+            if k.endswith("_model") and k != "make_model"
+        }
+    )
+    return models
+
+
+def construct_modelchains(system: models.PVSystem) -> List[ModelChain]:
+    """Construct a pvlib.modelchain.ModelChain object for each Inverter
+    in system"""
+    location = construct_location(system=system)
+    out = []
+    for i, inverter in enumerate(system.inverters):
+        pvsystem = construct_pvsystem(inverter=inverter, albedo=system.albedo)
+        mc = ModelChain(
+            system=pvsystem, location=location, **extract_pvlib_models(inverter)
+        )
+        out.append(mc)
+    return out
---
+++ api/solarperformanceinsight_api/tests/test_pvmodeling.py
@@ -0,0 +1,119 @@
+from inspect import signature
+
+
+from pvlib.location import Location
+from pvlib.modelchain import ModelChain
+from pvlib.pvsystem import PVSystem
+from pvlib.tracking import SingleAxisTracker
+import pytest
+
+
+from solarperformanceinsight_api import models, pvmodeling
+
+
+def test_construct_location(system_def):
+    # verify that location construction does no input validation
+    system_def.longitude = "notanumber"
+    assert isinstance(pvmodeling.construct_location(system_def), Location)
+
+
+@pytest.fixture()
+def fixed_tracking():
+    return models.FixedTracking(tilt=32, azimuth=180.9)
+
+
+def test_construct_tracker_fixed(fixed_tracking):
+    obj, od = pvmodeling.construct_tracker(fixed_tracking)
+    assert obj == PVSystem
+    assert od == {"surface_tilt": 32.0, "surface_azimuth": 180.9}
+
+
+@pytest.fixture()
+def single_axis_tracking():
+    return models.SingleAxisTracking(
+        axis_tilt=0, axis_azimuth=179.8, backtracking=False, gcr=1.8
+    )
+
+
+def test_construct_tracker_single(single_axis_tracking):
+    obj, od = pvmodeling.construct_tracker(single_axis_tracking)
+    assert obj == SingleAxisTracker
+    assert od == {
+        "axis_tilt": 0.0,
+        "axis_azimuth": 179.8,
+        "backtrack": False,
+        "gcr": 1.8,
+    }
+
+
+def test_construct_tracker_err(system_def):
+    with pytest.raises(TypeError):
+        pvmodeling.construct_tracker(system_def)
+
+
+@pytest.fixture(params=["fixed", "single"])
+def either_tracker(request, system_def, fixed_tracking, single_axis_tracking):
+    inv = system_def.inverters[0]
+    if request.param == "fixed":
+        inv.arrays[0].tracking = fixed_tracking
+        return inv, PVSystem
+    else:
+        inv.arrays[0].tracking = single_axis_tracking
+        return inv, SingleAxisTracker
+
+
+def test_construct_pvsystem(either_tracker):
+    inv, cls = either_tracker
+    out = pvmodeling.construct_pvsystem(inv, 0)
+    assert isinstance(out, cls)
+    assert isinstance(out.module_parameters, dict)
+    assert isinstance(out.temperature_model_parameters, dict)
+    assert isinstance(out.inverter_parameters, dict)
+
+
+def test_construct_pvsystem_consistent_kwargs_fixed(system_def, mocker, fixed_tracking):
+    pvsys = mocker.spy(pvmodeling, "PVSystem")
+    inv = system_def.inverters[0]
+    inv.arrays[0].tracking = fixed_tracking
+    out = pvmodeling.construct_pvsystem(inv, 0)
+    assert isinstance(out, PVSystem)
+    sig = signature(PVSystem)
+    params = set(sig.parameters.keys())
+    kwargs = set(pvsys.call_args.kwargs.keys())
+    assert kwargs.issubset(params)
+
+
+def test_extract_pvlib_models(system_def):
+    out = pvmodeling.extract_pvlib_models(system_def.inverters[0])
+    assert out == {
+        "ac_model": "sandia",
+        "dc_model": "pvsyst",
+        "temperature_model": "pvsyst",
+        "airmass_model": "kastenyoung1989",
+        "aoi_model": "no_loss",
+        "clearsky_model": "ineichen",
+        "spectral_model": "no_loss",
+        "transposition_model": "haydavies",
+        "losses_model": "pvwatts",
+    }
+
+
+def test_extract_pvlib_models_consistent_with_modelchain(system_def):
+    models = pvmodeling.extract_pvlib_models(system_def.inverters[0])
+    sig = signature(ModelChain)
+    model_params = {k for k in sig.parameters.keys() if k.endswith("_model")}
+    assert set(models.keys()) == model_params
+
+
+def test_construct_modelchains_fixed(system_def, fixed_tracking):
+    system_def.inverters[0].arrays[0].tracking = fixed_tracking
+    out = pvmodeling.construct_modelchains(system_def)
+    assert len(out) == 1
+    assert isinstance(out[0].system, PVSystem)
+
+
+def test_construct_modelchains_single(system_def, single_axis_tracking):
+    system_def.inverters[0].arrays[0].tracking = single_axis_tracking
+    out = pvmodeling.construct_modelchains(system_def)
+    assert len(out) == 1
+    assert isinstance(out[0].system, SingleAxisTracker)
