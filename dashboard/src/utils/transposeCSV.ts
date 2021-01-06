2713e86f5d0c76e4543df5b7fc931179a650cad9
Leland Boeman <lboeman@email.arizona.edu>
2021-01-06 15:19:54 -0700

    raise error when rows missing data

--- dashboard/src/utils/transposeCSV.ts
+++ dashboard/src/utils/transposeCSV.ts
@@ -4,7 +4,7 @@ export default async function(csvString: string) {
   /* Parses a csv file into an object mapping csv headers to column data for
    * easier creation of partial csv files.
    */
-  const csvJson = await csvToJson().fromString(csvString);
+  const csvJson = await csvToJson({ checkColumn: true }).fromString(csvString);
   const transposed: Record<string, Array<any>> = {};
   const headers: Array<string> = Object.keys(csvJson[0]);
   for (const header of headers) {
