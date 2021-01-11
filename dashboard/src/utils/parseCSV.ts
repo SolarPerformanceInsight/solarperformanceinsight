a753dc144afa05fa5c98d8a22771ef565c46bd07
Tony Lorenzo <atlorenzo@email.arizona.edu>
2021-01-11 10:29:24 -0700

    fix types for build and add build setp as part of action testing

--- dashboard/src/utils/parseCSV.ts
+++ dashboard/src/utils/parseCSV.ts
@@ -1,7 +1,13 @@
 import Papa from "papaparse";
 
-export default function(csvString: string, previewLines = 0) {
-  const parsed = Papa.parse(csvString, {
+export default function(
+  csvString: string,
+  previewLines = 0
+): Papa.ParseResult<Record<string, Array<string | number>>> {
+  const parsed: Papa.ParseResult<Record<
+    string,
+    Array<string | number>
+  >> = Papa.parse(csvString, {
     header: true,
     dynamicTyping: true,
     preview: previewLines
