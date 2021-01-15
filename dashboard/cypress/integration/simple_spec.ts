4d1f123aa83244cf8bf9412e0afc03fd2c687716
lboeman <lboeman@email.arizona.edu>
2021-01-15 15:35:46 -0700

    correct system/new url

--- dashboard/cypress/integration/simple_spec.ts
+++ dashboard/cypress/integration/simple_spec.ts
@@ -7,14 +7,14 @@ describe("Test Dash", () => {
   it("Connect to systems", () => {
     cy.login(Cypress.env("auth_username"), Cypress.env("auth_password"));
 
-    cy.visit("http://localhost:8001/systems");
+    cy.visit("http://localhost:8001");
 
-    cy.get("h1").contains("Systems");
+    cy.contains("Successfully logged in.");
   });
   it("Connect to new System", () => {
     cy.login(Cypress.env("auth_username"), Cypress.env("auth_password"));
 
-    cy.visit("http://localhost:8001/systems/new");
+    cy.visit("http://localhost:8001/system/new");
 
     cy.get("h1").contains("New System");
   });
