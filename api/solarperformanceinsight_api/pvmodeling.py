9477120b019a17fa263ec86eab32973a4a70d904
Tony Lorenzo <atlorenzo@email.arizona.edu>
2021-01-22 12:32:37 -0700

    use pvlib.Arrays and lift the requirement of only a single array (#92)
    partially addresses #52, but still need to avoid multiple arrays for
    single axis tracking

--- api/solarperformanceinsight_api/pvmodeling.py
+++ api/solarperformanceinsight_api/pvmodeling.py
@@ -1,12 +1,12 @@
 """Module to translate models.py into pvlib objects and to run the
 pvlib models.
 """
-from typing import Union, Tuple, Any, Dict, List
+from typing import Union, List
 
 
 from pvlib.location import Location  # type: ignore
 from pvlib.modelchain import ModelChain  # type: ignore
-from pvlib.pvsystem import PVSystem  # type: ignore
+from pvlib.pvsystem import PVSystem, Array  # type: ignore
 from pvlib.tracking import SingleAxisTracker  # type: ignore
 
 from . import models
@@ -22,47 +22,68 @@ def construct_location(system: models.PVSystem) -> Location:
     )
 
 
-def construct_tracker(
-    tracking: Union[models.FixedTracking, models.SingleAxisTracking]
-) -> Tuple[Union[PVSystem, SingleAxisTracker], Dict[str, Any]]:
-    """Return the appropriate pvlib.pvsystem class and tracking keywords
-    depending on the type of tracking unit"""
-    if isinstance(tracking, models.FixedTracking):
-        return PVSystem, dict(
-            surface_tilt=tracking.tilt, surface_azimuth=tracking.azimuth
-        )
-    elif isinstance(tracking, models.SingleAxisTracking):
-        return SingleAxisTracker, dict(
-            axis_tilt=tracking.axis_tilt,
-            axis_azimuth=tracking.axis_azimuth,
-            gcr=tracking.gcr,
-            backtrack=tracking.backtracking,
-        )
-    else:
-        raise TypeError("Only FixedTracking and SingleAxisTracking currently supported")
-
-
 def construct_pvsystem(inverter: models.Inverter) -> Union[PVSystem, SingleAxisTracker]:
     """Construct a pvlib.pvsystem.PVSystem (or SingleAxisTracker) from an
     Inverter object"""
-    # only a single array for now until pvlib#1076
-    # otherwise one pvsystem/pvarray per array
-    array = inverter.arrays[0]
-    common_kwargs = dict(
-        albedo=array.albedo,
-        module=array.make_model,
-        module_parameters=array.module_parameters.dict(),
-        temperature_model_parameters=array.temperature_model_parameters.dict(),
-        modules_per_string=array.modules_per_string,
-        strings_per_inverter=array.strings,
+    system_kwargs = dict(
         inverter=inverter.make_model,
         inverter_parameters=inverter.inverter_parameters.dict(),
         name=inverter.name,
     )
     if inverter.losses is not None:
-        common_kwargs["losses_parameters"] = inverter.losses.dict()
-    system_model, tracking_kwargs = construct_tracker(array.tracking)
-    return system_model(**tracking_kwargs, **common_kwargs)
+        system_kwargs["losses_parameters"] = inverter.losses.dict()
+
+    array_params = []
+    tracking_params = []
+    is_single_axis = False
+    for array in inverter.arrays:
+        array_params.append(
+            dict(
+                albedo=array.albedo,
+                module=array.make_model,
+                module_parameters=array.module_parameters.dict(),
+                temperature_model_parameters=array.temperature_model_parameters.dict(),
+                modules_per_string=array.modules_per_string,
+                strings=array.strings,
+                name=array.name,
+            )
+        )
+        if isinstance(array.tracking, models.SingleAxisTracking):
+            tracking_params.append(
+                dict(
+                    axis_tilt=array.tracking.axis_tilt,
+                    axis_azimuth=array.tracking.axis_azimuth,
+                    gcr=array.tracking.gcr,
+                    backtrack=array.tracking.backtracking,
+                )
+            )
+            is_single_axis = True
+            # later might also need to keep track of the array class to use
+        else:
+            tracking_params.append(
+                dict(
+                    surface_tilt=array.tracking.tilt,
+                    surface_azimuth=array.tracking.azimuth,
+                )
+            )
+
+    if is_single_axis:
+        if len(tracking_params) > 1:  # pragma: no cover
+            # until pvlib/pvlib-python#1109 is resolved
+            raise ValueError("Single axis tracking with multiple arrays not supported")
+        else:
+            return SingleAxisTracker(
+                arrays=[
+                    Array(**array_params[0], surface_tilt=None, surface_azimuth=None)
+                ],
+                **tracking_params[0],
+                **system_kwargs
+            )
+    else:
+        system_kwargs["arrays"] = [
+            Array(**atp[0], **atp[1]) for atp in zip(array_params, tracking_params)
+        ]
+        return PVSystem(**system_kwargs)
 
 
 def construct_modelchains(system: models.PVSystem) -> List[ModelChain]:
