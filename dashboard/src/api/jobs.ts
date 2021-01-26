27b55346ea25a1e698cd79d6d66e050813a8ef7b
Leland Boeman <lboeman@email.arizona.edu>
2021-01-26 14:15:12 -0700

    add get job results api functions

--- dashboard/src/api/jobs.ts
+++ dashboard/src/api/jobs.ts
@@ -17,16 +17,26 @@ async function jobsRequest(
 export async function create(token: string, job: Record<string, any>) {
   return jobsRequest(token, "post", null, JSON.stringify(job));
 }
-export async function read(token: string, jobid: string) {
-  return jobsRequest(token, "get", null, null, `${jobid}`);
+export async function read(token: string, jobId: string) {
+  return jobsRequest(token, "get", null, null, `${jobId}`);
 }
 export async function addData(
   token: string,
-  jobid: string,
-  dataid: string,
+  jobId: string,
+  dataId: string,
   csv: string
 ) {
   const data = new FormData();
   data.append("file", new Blob([csv], { type: "text/csv" }));
-  return jobsRequest(token, "post", null, data, `${jobid}/data/${dataid}`);
+  return jobsRequest(token, "post", null, data, `${jobId}/data/${dataId}`);
+}
+export async function getResults(token: string, jobId: string) {
+  return jobsRequest(token, "get", null, null, `${jobId}/results`);
+}
+export async function getSingleResults(
+  token: string,
+  jobId: string,
+  resultId: string
+) {
+  return jobsRequest(token, "get", null, null, `${jobId}/results/${resultId}`);
 }
