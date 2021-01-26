9d9d364c4d43160ce19dae36416b0ffb33f3367b
Leland Boeman <lboeman@email.arizona.edu>
2021-01-25 17:02:29 -0700

    more lint

--- dashboard/tests/unit/utils/test_elevation.spec.ts
+++ dashboard/tests/unit/utils/test_elevation.spec.ts
@@ -1,6 +1,5 @@
 import { getElevation } from "@/utils/elevation";
 
-
 let mockResponse: any = {};
 let fetchMock: any = {};
 global.fetch = jest.fn(() => Promise.resolve(fetchMock));
@@ -13,7 +12,7 @@ beforeEach(() => {
         Elevation: 722.68,
         Units: "Meters",
         x: -110.9747,
-        y: 32.2226,
+        y: 32.2226
       }
     }
   };
@@ -25,20 +24,18 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-
 describe("test elevation", () => {
-  it("test success", async() => {
+  it("test success", async () => {
     const el = await getElevation(32.2226, -110.9747);
     expect(el).toBe(722.68);
   });
-  it("test missing value", async() => {
-     mockResponse
-       .USGS_Elevation_Point_Query_Service
-       .Elevation_Query.Elevation = "-1000000";
-     expect(getElevation(32.2226, -110.9747)).rejects.toThrow();
+  it("test missing value", async () => {
+    mockResponse.USGS_Elevation_Point_Query_Service.Elevation_Query.Elevation =
+      "-1000000";
+    expect(getElevation(32.2226, -110.9747)).rejects.toThrow();
   });
-  it("test request failure", async() => {
-     fetchMock.ok = false;
-     expect(getElevation(32.2226, -110.9747)).rejects.toThrow();
+  it("test request failure", async () => {
+    fetchMock.ok = false;
+    expect(getElevation(32.2226, -110.9747)).rejects.toThrow();
   });
 });
