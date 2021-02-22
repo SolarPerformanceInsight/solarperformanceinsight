bb63ff31feb79d5a426227712f6bff0d0fe46caa
Leland Boeman <lboeman@email.arizona.edu>
2021-02-22 13:17:56 -0700

    test converter

--- dashboard/tests/unit/utils/test_power_conversion.spec.ts
+++ dashboard/tests/unit/utils/test_power_conversion.spec.ts
@@ -1,7 +1,7 @@
 import convertPower from "@/utils/powerConversion";
 
 describe("Test power conversion", () => {
-  it("kW => w", () => {
+  it("kW => W", () => {
     const expected = [
       [1, 1000],
       [10, 10000],
@@ -10,8 +10,64 @@ describe("Test power conversion", () => {
     ];
     for (const results of expected) {
       expect(
-
-      ).toBe(
+        convertPower(
+          "kW",
+          "W",
+          results[0]
+        )
+      ).toBeCloseTo(results[1]);
+    }
+  });
+  it("MW => W", () => {
+    const expected = [
+      [1, 1000000],
+      [10, 10000000],
+      [.25, 250000],
+      [.001, 1000]
+    ];
+    for (const results of expected) {
+      expect(
+        convertPower(
+          "MW",
+          "W",
+          results[0]
+        )
+      ).toBeCloseTo(results[1]);
+    }
+  });
+  it("GW => W", () => {
+    const expected = [
+      [1, 1000000000],
+      [10, 10000000000],
+      [.25, 250000000],
+      [.001, 1000000]
+    ];
+    for (const results of expected) {
+      expect(
+        convertPower(
+          "GW",
+          "W",
+          results[0]
+        )
+      ).toBeCloseTo(results[1]);
+    }
+  });
+  it("W => MW", () => {
+    const expected = [
+      [1000000, 1],
+      [10000000, 10],
+      [250000, .25],
+      [1000, .001],
+      [5, .000005]
+    ];
+    for (const results of expected) {
+      expect(
+        convertPower(
+          "W",
+          "MW",
+          results[0]
+        )
+      ).toBeCloseTo(results[1]);
     }
   });
 });
