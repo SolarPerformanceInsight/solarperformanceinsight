bc4311f5c610ff32436dee0dd0baf025d930bb1b
Leland Boeman <lboeman@email.arizona.edu>
2021-02-22 10:31:39 -0700

    start test

--- dashboard/src/utils/powerConversion.ts
+++ dashboard/src/utils/powerConversion.ts
@@ -1,9 +1,17 @@
-const unitValues = {
+const unitValues: Record<string, number> = {
   W: 1,
   kW: 1000,
   MW: 1000000,
   GW: 1000000000,
 }
 
-export function convertPower(origUnits, targetUnits, value) {
+export default function convertPower(
+  origUnits: string,
+  targetUnits: string,
+  value: number
+) {
+  const orig = unitValues[origUnits];
+  const target = unitValues[targetUnits];
+  const factor = orig / target;
+  return value * factor;
 }
---
+++ dashboard/tests/unit/utils/test_power_conversion.spec.ts
@@ -0,0 +1,17 @@
+import convertPower from "@/utils/powerConversion";
+
+describe("Test power conversion", () => {
+  it("kW => w", () => {
+    const expected = [
+      [1, 1000],
+      [10, 10000],
+      [.25, 250],
+      [.001, 1]
+    ];
+    for (const results of expected) {
+      expect(
+
+      ).toBe(
+    }
+  });
+});
