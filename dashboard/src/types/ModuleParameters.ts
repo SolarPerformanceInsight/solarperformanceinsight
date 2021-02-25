37a57039fdf4bb571a07f8b02eb53b1b59033503
Leland Boeman <lboeman@email.arizona.edu>
2021-02-25 09:43:00 -0700

    add CECModuleParameters type

--- dashboard/src/types/ModuleParameters.ts
+++ dashboard/src/types/ModuleParameters.ts
@@ -68,3 +68,54 @@ export class PVWattsModuleParameters {
     return maybe.pdc0 != undefined && maybe.gamma_pdc != undefined;
   }
 }
+export class CECModuleParameters {
+  alpha_sc: number;
+  a_ref: number;
+  I_L_ref: number;
+  I_o_ref: number;
+  R_sh_ref: number;
+  R_s: number;
+  cells_in_series: number;
+  Adjust: number;
+  EgRef: number;
+  dEgdT: number;
+
+  constructor({
+    alpha_sc = 0,
+    a_ref = 0,
+    I_L_ref = 0,
+    I_o_ref = 0,
+    R_sh_ref = 0,
+    R_s = 0,
+    cells_in_series = 0,
+    Adjust = 0,
+    EgRef = 1.121,
+    dEgdT = -0.0002677
+  }: Partial<CECModuleParameters>) {
+    this.alpha_sc = alpha_sc;
+    this.a_ref = a_ref;
+    this.I_L_ref = I_L_ref;
+    this.I_o_ref = I_o_ref;
+    this.R_sh_ref = R_sh_ref;
+    this.R_s = R_s;
+    this.cells_in_series = cells_in_series;
+    this.Adjust = Adjust;
+    this.EgRef = EgRef;
+    this.dEgdT = dEgdT;
+  }
+  static isInstance(obj: any): obj is CECModuleParameters {
+    const maybe = obj as CECModuleParameters;
+    return (
+      maybe.alpha_sc != undefined &&
+      maybe.a_ref != undefined &&
+      maybe.I_L_ref != undefined &&
+      maybe.I_o_ref != undefined &&
+      maybe.R_sh_ref != undefined &&
+      maybe.R_s != undefined &&
+      maybe.cells_in_series != undefined &&
+      maybe.Adjust != undefined &&
+      maybe.EgRef != undefined &&
+      maybe.dEgdT != undefined
+    );
+  }
+}
