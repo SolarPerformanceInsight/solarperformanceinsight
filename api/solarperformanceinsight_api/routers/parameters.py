a409a3d85d30ea7e716a3ccc1494adb7d3e3f92c
Leland Boeman <lboeman@email.arizona.edu>
2021-02-26 07:17:56 -0700

    Apply suggestions from code review
    Co-authored-by: Tony Lorenzo <atlorenzo@email.arizona.edu>

--- api/solarperformanceinsight_api/routers/parameters.py
+++ api/solarperformanceinsight_api/routers/parameters.py
@@ -19,8 +19,8 @@ sandia_inverter_params = (
 
 # Database does not provide the fields 'cells_in_series', 'EgRef', 'dEgdT'
 # which have defaults in the model.
-cec_keys = ["alpha_sc", "a_ref", "I_L_ref", "I_o_ref", "R_sh_ref", "R_s", "Adjust"]
-cec_module_params = retrieve_sam("CECMod").loc[cec_keys].astype(float)
+cec_keys = ["alpha_sc", "a_ref", "I_L_ref", "I_o_ref", "R_sh_ref", "R_s", "Adjust", "N_s"]
+cec_module_params = retrieve_sam("CECMod").loc[cec_keys].astype(float).rename(index={"N_s": "cells_in_series"})
 
 
 @router.get(
@@ -87,5 +87,5 @@ def get_cec_module(
             detail=f"Module parameters for {module_name} not found",
         )
     return models.CECModuleParameters(
-        **cec_module_params[module_name].to_dict(), cells_in_series=0
+        **cec_module_params[module_name].to_dict()
     )
