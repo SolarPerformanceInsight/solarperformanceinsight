8b4d03b14d362cdc12f6e69abf06e7b3eb7d245c
Leland Boeman <lboeman@email.arizona.edu>
2021-03-05 14:20:50 -0700

    coverage

--- dashboard/tests/unit/jobs/test_timeparameters.spec.ts
+++ dashboard/tests/unit/jobs/test_timeparameters.spec.ts
@@ -61,4 +61,51 @@ describe("test timeparameters", () => {
       }
     ]);
   });
+  it("test start after end", async () => {
+    const wrapper = mount(JobTimeParameters, {
+      localVue
+    });
+    await flushPromises();
+
+    // @ts-expect-error
+    expect(wrapper.emitted("new-timeparams")[0]).toEqual([
+      {
+        start: null,
+        end: null,
+        step: 3600,
+        timezone: "America/Denver"
+      }
+    ]);
+    const startend = wrapper.findAllComponents(DatetimeField);
+    const start = startend.wrappers[0];
+    start.vm.$data.year = 2022;
+    start.vm.$data.month = 1;
+    start.vm.$data.day = 1;
+    start.vm.$data.hour = 0;
+    start.vm.$data.minute = 0;
+
+    // @ts-expect-error
+    start.vm.emitTimeParams();
+    await flushPromises();
+
+    const end = startend.wrappers[1];
+
+    end.vm.$data.year = 2020;
+    end.vm.$data.month = 2;
+    end.vm.$data.day = 1;
+    end.vm.$data.hour = 0;
+    end.vm.$data.minute = 0;
+
+    // @ts-expect-error
+    end.vm.emitTimeParams();
+    await flushPromises();
+
+    // @ts-expect-error
+    wrapper.vm.emitParams();
+    await flushPromises();
+
+    expect(wrapper.find("li").text()).toBe(
+      "Start End:\n        Start must be before End."
+    );
+  });
 });
