e808837c755e9c3e7606ef05948459a20564e663
lboeman <lboeman@email.arizona.edu>
2021-03-29 13:47:54 -0700

    maintain modified_at time

--- db/migrations/20210326144800_terminology_update.sql
+++ db/migrations/20210326144800_terminology_update.sql
@@ -29,7 +29,7 @@ begin
     '$.parameters.compare', 'modeled and actual performance')
   where JSON_EXTRACT(definition, "$.parameters.compare") = "expected and actual performance";
 
-  update jobs set definiton = JSON_INSERT(
+  update jobs set definition = JSON_INSERT(
     JSON_REMOVE(definition, '$.parameters.predicted_data_parameters'),
     '$.parameters.reference_data_parameters',
     JSON_EXTRACT(definition, '$.parameters.predicted_data_parameters')
@@ -41,6 +41,11 @@ begin
   update job_data set type = 'reference monthly performance data' where type = 'predicted monthly performance data';
   update job_data set type = 'modeled performance data' where type = 'expected performance data';
 
+  -- ensure jobs added in add_example_data 1 and 3 retain their modified_at time
+  set @extime = timestamp('2020-12-11 19:52');
+  set @jobid = uuid_to_bin('e1772e64-43ac-11eb-92c2-f4939feddd82', 1);
+  set @otherjobid = uuid_to_bin('7f13ab34-43ad-11eb-80a2-f4939feddd82', 1);
+  update jobs set modified_at = @extime where id in (@jobid, @otherjobid);
 end;
 
 drop procedure add_example_data;
@@ -57,7 +62,7 @@ begin
   CALL _add_example_data_7;
 end;
 
--- migration:down
+-- migrate:down
 drop procedure add_example_data;
 create procedure add_example_data ()
   modifies sql data
