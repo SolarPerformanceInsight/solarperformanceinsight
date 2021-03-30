22a8947bb4e481807bd912aa7fcd14546f715a3c
lboeman <lboeman@email.arizona.edu>
2021-03-30 09:03:51 -0700

    comment edits

--- dashboard/src/components/jobs/parameters/JobParams.vue
+++ dashboard/src/components/jobs/parameters/JobParams.vue
@@ -10,8 +10,8 @@
         <div class="my-1">
           <form id="job-parameters" @submit="submitJob">
             <!-- For selecting between diferent usecase variants e.g. CompareJobParams
-                 Allows user to select betweel reference-actual, modeled-actual, and
-                 reference modeled.
+                 Allows user to select between reference-actual, modeled-actual, and
+                 reference-modeled.
             -->
             <component
               v-bind:is="jobParamComponent"
